3d6e4e67a75caad4b61d50fc66b0f5a8286b8c4f
Aca-S <a.stefanovic1999@gmail.com>
2024-08-07 00:28:03 +0200

    Modified the coreutils build script to accept an optional llvm tool version suffix

--- scripts/setup_coreutils.sh
+++ scripts/setup_coreutils.sh
@@ -14,17 +14,29 @@ build_llvm ()
 {
     llvm_dir="${1}/obj-llvm"
     rm -rf $llvm_dir
+
+    cc_name=clang
+    linker_name=llvm-link
+
+    if [ "$2" ]; then
+        cc_name+="-${2}"
+        linker_name+="-${2}"
+    fi
+
     mkdir $llvm_dir
     (
         cd $llvm_dir
+        export LLVM_CC_NAME=$cc_name
         export LLVM_COMPILER=clang
         CC=wllvm ../configure --disable-nls CFLAGS="-g -O1 -Xclang -disable-llvm-passes -D__NO_STRING_INLINES  -D_FORTIFY_SOURCE=0 -U__OPTIMIZE__"
         make
-        find . -executable -type f | xargs -I '{}' extract-bc '{}'
+        find . -executable -type f ! -name 'config.status' | xargs -I '{}' extract-bc -l $linker_name '{}'
     )
 }
 
 cu_dir="../coreutils"
 
 bootstrap_coreutils $cu_dir
-build_llvm $cu_dir
+build_llvm $cu_dir 13
+
+echo 'All done!'
